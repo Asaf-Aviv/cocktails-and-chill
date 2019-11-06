0fcf99efbbda7b3194fe2e9802f4d57e65ff1d3e
Asaf Aviv <asafaviv89@gmail.com>
2019-11-07 01:34:48 +0200

    disable x-powered-by

--- server/app/app.js
+++ server/app/app.js
@@ -8,6 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const devMode = process.env.NODE_ENV !== 'production';
 
+app.disable('x-powered-by');
+
 app.use(morgan(devMode ? 'dev' : 'combined'));
 
 app.use(express.static(path.join(__dirname, '../../client/build')));
